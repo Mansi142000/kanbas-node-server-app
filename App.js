943dcec1e92934dd5bf5d0b045ac3ea55667abc0
Mansi142000 <155905751+Mansi142000@users.noreply.github.com>
2024-08-12 22:34:23 -0400

    json parser

--- App.js
+++ App.js
@@ -34,8 +34,7 @@ if (process.env.NODE_ENV !== "development") {
 }
 app.use(session(sessionOptions));
 
-app.use(session(sessionOptions));
-app.use(express.json());
+
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
